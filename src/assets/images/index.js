ec89415b9b51723f6ced2f1d89a998df31eda6e6
theo.borella.hc@gmail.com <theo.borella.hc@gmail.com>
2025-05-06 14:48:52 +0200

    chae

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,7 @@
 import hero from './hero.jpg'
 import logo from './logo.svg'
 import lbalogo from './lbalogo.png'
+import liongency from './liongency.png'
 import evolutive from './evolutive.png'
 import avatar from './avatar.png'
 import php from './php.png'
@@ -9,6 +10,7 @@ export {
     hero,
     logo,
     lbalogo,
+    liongency,
     evolutive,
     avatar,
     php
